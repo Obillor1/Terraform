6b0ed2d3f4ad190aaa00d9fa4c59a86039dbb01a
Obillor1 <129910344+Obillor1@users.noreply.github.com>
2025-07-14 00:11:39 -0600

    Update ec2_bastion_variable.tf

--- Module/ec2_bastion_variable.tf
+++ Module/ec2_bastion_variable.tf
@@ -14,3 +14,24 @@ variable "instance_keypair" {
   type = string
   default = "Ohio_KeyPAir"
 }
+
+
+# Input Variables
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "us-east-2"  
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "SAP"
+}
