a4c0b6288b4de1109f29dfbcdebc020d08301f05
Obillor1 <129910344+Obillor1@users.noreply.github.com>
2025-07-14 00:10:23 -0600

    Create ec2.bastion_security.tf

---
+++ Module/ec2.bastion_security.tf
@@ -0,0 +1,15 @@
+module "public_bastion_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name        = "${local.name}-public_bastion_sg"
+  description = "Security group for SSH Port open for IPv4 CIDR, egress Port is open for everyone"
+  vpc_id      = module.vpc.vpc_id
+
+#Ingress Rules
+  ingress_rules = ["https-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+#Egress Rules & CIDR ALL-ALL Open
+    egress_rules = ["all-all"] #This is in the inputs attribute of the terraform module
+    tags = local.common_tags
+}
