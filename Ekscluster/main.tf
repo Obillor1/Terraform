643f869ccbb8cacb44876a845e4d1647c2917461
Obillor1 <129910344+Obillor1@users.noreply.github.com>
2025-07-15 05:00:48 -0600

    Update main.tf

--- Ekscluster/main.tf
+++ Ekscluster/main.tf
@@ -1,1 +1,7 @@
+#Dependency Resources
+# VPN - Subnets(Public and Private) - Route Tables - Nat gateway + Elastic IP - Internet Gateway
+
+#EKS RESOURCES - 
+# Eks Cluster - EKS Cluste IAM Role - EKS Cluster Security GRoup - EKS cluster Network Interfaces
+#EKS NODE GROUP - Same as above
 
